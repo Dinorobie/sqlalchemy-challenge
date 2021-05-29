4b0de31d1e90bca259ee25f15ad9e2fca87a5b87
Robert Sepulveda <robert@MacBook-Pro.local>
2021-05-29 18:21:22 -0500

    Add files

---
+++ Resources/.ipynb_checkpoints/app.py
@@ -0,0 +1,84 @@
+#
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///hawaii.sqlite")
+conn = engine.connect()
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# View all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Aloha this is Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all Stations"""
+    # Query all Stations
+    results = session.query(Station.station).\
+                 order_by(Station.station).all()
+
+    session.close()
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+
+    # Dict with date as the key and prcp as the value
+    preci = {date: prcp for date, prcp in precipitation}
+    return jsonify(preci)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    """Return the temperature observations (tobs) for previous year."""
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the primary station for all tobs from the last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+
+if __name__ == "__main__":
+    app.run(debug=True)
